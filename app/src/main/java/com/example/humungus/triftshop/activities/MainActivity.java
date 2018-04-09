b9bc527af675cb6000192e7f6339325355986e8b
mutegi <mutegisamuel0@gmail.com>
2018-04-09 14:06:43 +0300

    1.1 update

--- app/src/main/java/com/example/humungus/triftshop/activities/MainActivity.java
+++ app/src/main/java/com/example/humungus/triftshop/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.humungus.triftshop.activities;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,12 @@ import com.example.humungus.triftshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.navDrawer);
     }
 }
